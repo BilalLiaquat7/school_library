1913ec1f096440789368757b57fdcaf7ae6f4ad0
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-10-12 18:01:30 +0500

    fixes done as per reviewer asked

--- person.rb
+++ person.rb
@@ -2,12 +2,12 @@ require './nameable'
 require './capitalize_decorator'
 require './trimmer_decorator'
 
-class Person
-  # include Nameable
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @age = age
     @name = name
