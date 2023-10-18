53ac50aae7ab7e9a872bb9c41b012c136b34f5e9
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-10-18 21:22:02 +0500

    functionality completed

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require './person'
 
 class Teacher < Person
   def initialize(specialization = 'Unknown', age = 0, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
